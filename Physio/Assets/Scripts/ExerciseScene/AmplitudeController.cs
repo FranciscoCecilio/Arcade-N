7d2717c2c5fd7d32894cfbe31c4f1f306db0914b
Francisco Cecilio <ist189444@tecnico.ulisboa.pt>
2022-02-23 13:05:38 +0000

    Created BodyWrapper.cs and setup in Exercise1Scene

--- Physio/Assets/Scripts/ExerciseScene/AmplitudeController.cs
+++ Physio/Assets/Scripts/ExerciseScene/AmplitudeController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class AmplitudeController : MonoBehaviour {
 
+    //public GameObject patient;
     public BodyWrapper patient;
     public float maxSXY = 0;
     public float maxSXZ = 0;
@@ -31,7 +32,7 @@ public class AmplitudeController : MonoBehaviour {
     public Text cuElbow;
     public Text cuSup;
     public Text cuSdown;
-
+    
 
     // Use this for initialization
     void Start () {
@@ -46,9 +47,7 @@ public class AmplitudeController : MonoBehaviour {
             ShoulderXY();
             ShoulderXZ();
             Elbow();
-        }
-
-       
+        }       
 	}
 
 
@@ -181,16 +180,6 @@ public class AmplitudeController : MonoBehaviour {
                 break;
 
         }
-
-
-
-
-
-
   
     }
-
-
-
-
 }
